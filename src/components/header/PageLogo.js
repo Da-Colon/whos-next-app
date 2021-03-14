cb9a8b342758d7b8528ceb76793c118cff671c4a
David Colon <dcolonantonio@gmail.com>
2021-03-14 14:04:14 -0400

    added logo - created logo with figma - inspired by tyche symbol

--- src/components/header/PageLogo.js
+++ src/components/header/PageLogo.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '../assets/images/logo-white.svg'
 
 const PageLogo = () => {
   return (
-    <div className="h-16 w-16 border border-white">
-
+    <div className="flex justify-center items-center">
+      <img alt="" src={logo} />
+      <div className="text-white text-lg font-bold ml-2">Who's next?</div>
     </div>
   )
 }
