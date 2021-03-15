51f796a421ead1ce0269169c4e3b4baed7050e0d
David Colon <dcolonantonio@gmail.com>
2021-03-15 18:19:19 -0400

    - created new varient 'noMinHeight'

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -2,8 +2,15 @@ import React from 'react'
 import classnames from 'classnames'
 
 const BASE = "flex flex-col mt-8 pt-8 background-main inset-shadow"
-const Container = ({ padTop, position, children }) => {
-  return (
+const BASE_NO_MIN_HEIGHT = "flex flex-col p-8 pt-4 background-main inset-shadow"
+const Container = ({ padTop, position, varient, children }) => {
+  if(varient === 'noMinHeight') {
+    return (
+      <div className={classnames(BASE_NO_MIN_HEIGHT, position) }>
+        {children}
+      </div>
+    )
+  } else return (
     <div className={classnames(BASE, position)} style={{minHeight: `calc(100vh - ${padTop}`}}>
       {children}
     </div>
