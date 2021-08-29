72c2fa743a42ea8c7894aa4e67c4bf4767995813
David Colon <dcolonantonio@gmail.com>
2021-08-29 10:22:02 -0400

    reset form on route change away from new list

--- src/components/lists/CreateNewList/CreateListSteps.tsx
+++ src/components/lists/CreateNewList/CreateListSteps.tsx
@@ -7,12 +7,23 @@ import CreationMethod from "./CreationMethod";
 import ListCreation from "./ListCreation";
 import NameAndSettings from "./NameAndSettings";
 import Review from "./Review";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { ClientRoutes } from "../../../router/routes";
 
 const CreateListSteps: FC<IListSteps> = (props) => {
   const listsStore: IListStore = useListData();
+  const location = useLocation();
   const [manualListLength, setManualListLength] = useState<number>(0);
   // ! This will be used later for storing uploaded list data
   // const [ uploadedList, setUploadedList] = useState();
+
+  useEffect(() => {
+    if (location.pathname !== ClientRoutes.listsNew) {
+      listsStore.updateCreateListState(ECreateListSteps.NameAndSettings);
+      props.resetForm();
+    }
+  });
 
   const updateManualListLength = (length: number) => {
     setManualListLength(length);
@@ -36,7 +47,7 @@ const CreateListSteps: FC<IListSteps> = (props) => {
       return <div></div>;
     }
     case ECreateListSteps.Review: {
-      return <Review  {...props} />;
+      return <Review {...props} />;
     }
   }
 };
